3f5260d033175c375c7982769cd67673c6d2fbe8
andrei <andrei@local>
2022-11-03 00:05:04 +0200

    creating tables for languages and translations

--- database/migrations/2022_11_02_214821_translations.php
+++ database/migrations/2022_11_02_214821_translations.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('translations', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->unsignedBigInteger('language_id')->comment('Language ID');
+            $table->string('key_name', 40)->comment('Name of the key');
+            $table->string('key_value')->comment('Value of the key');
+            $table->foreign('language_id')->references('id')->on('languages');
+            $table->unique('language_id', 'key_name');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::drop('translations');
     }
 };
